363e513ccab9f1e5fe4d6af9a4c25c621ae8e756
pennynd1me <4prilkim@gmail.com>
2021-03-15 21:13:40 +0900

    add domain package

---
+++ src/main/java/com/pennynd1me/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.pennynd1me.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long>{
+}
