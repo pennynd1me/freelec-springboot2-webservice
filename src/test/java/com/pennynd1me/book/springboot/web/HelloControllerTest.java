facd59dc3901e84050a76f2a0770107e9bf7781c
pennynd1me <4prilkim@gmail.com>
2021-04-05 23:46:32 +0900

    5장 완료

--- src/test/java/com/pennynd1me/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/pennynd1me/book/springboot/web/HelloControllerTest.java
@@ -1,28 +1,33 @@
 package com.pennynd1me.book.springboot.web;
 
-import com.pennynd1me.book.springboot.web.HelloController;
+import com.pennynd1me.book.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class) // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다.
 // 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 한다.
-@WebMvcTest(controllers = HelloController.class) //
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                classes = SecurityConfig.class)})
 public class HelloControllerTest {
 
     @Autowired //
     private MockMvc mvc; //
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -32,6 +37,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
